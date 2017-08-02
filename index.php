8ad21fec4c656848a4cdd0d07e773c1e506a5c7f
KOCHANG-PC\Kota <kochang2014@gmail.com>
2017-08-02 22:44:20 +0900

    352TOP追加

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="352, 猫, ネコ, ミコニ, 大塚, 保護, まどろむ, ねこ, 三毛猫, ネコちゃん">
+    <meta name="description" content="保護猫カフェ大塚シェルターで出会った、かわいい三毛猫ちゃんを保護して１年になります。352番の札をつけられていたことから「ミコニ」と名付けられました。お転婆でおしゃべりが大好き！">
+    <title>まどろむ猫ミコニ</title>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+        ga('create', 'UA-89130219-1', 'auto');
+        ga('set', 'dimension1', 'sessionCheck');
+        ga('send', 'pageview');
+    </script>
+</head>
+<body>
+    <h1>352 猫 ネコ ミコニ 大塚 保護 まどろむ ねこ 三毛猫 ネコちゃん</h1>
+    <p><a href="https://www.youtube.com/watch?v=QEOJQB7Cj6s">ミコニ</a>352 猫 ネコ 大塚 保護 まどろむ ねこ 三毛猫 ネコちゃん</p>
+    <p>
+        保護猫カフェ大塚シェルターで出会った、かわいい三毛猫ちゃんを保護して１年になります。<br>
+        352番の札をつけられていたことから「ミコニ」と名付けられました。お転婆でおしゃべりが大好き！
+    </p>
+    <p>
+        <a href="https://www.youtube.com/watch?v=QEOJQB7Cj6s">
+            <img src="./img/352.jpg">
+        </a>
+    </p>
+</body>
+</html>
