749f7cc0c5f83a66d59ecb01fd2201aa423d5b0a
KOCHANG-PC\Kota <kochang2014@gmail.com>
2017-08-05 00:19:52 +0900

    ディレクトリの構成変更とconstファイルを作成とその読み込み

---
+++ controller/constantList.php
@@ -0,0 +1,12 @@
+<?php
+
+$protocol = empty($_SERVER["HTTPS"]) ?"http://" :"https://";
+
+define("HOME_DIR", $protocol. $_SERVER['SERVER_NAME']);
+define("MODEL_DIR",      HOME_DIR. '/model');
+define("VIEW_DIR",       HOME_DIR. '/view');
+define("CONTROLLER_DIR", HOME_DIR. '/controller');
+define("CSS_DIR",        HOME_DIR. '/view/css');
+define("JS_DIR",         HOME_DIR. '/view/js');
+define("IMG_DIR",        HOME_DIR. '/view/js');
+
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+ini_set( 'display_errors', 1 );
+require_once(dirname(__FILE__) . '/controller/constantList.php');
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -5,10 +10,7 @@
     <meta name="keywords" content="352, 猫, ネコ, ミコニ, 大塚, 保護, まどろむ, ねこ, 三毛猫, ネコちゃん">
     <meta name="description" content="保護猫カフェ大塚シェルターで出会った、かわいい三毛猫ちゃんを保護して１年になります。352番の札をつけられていたことから「ミコニ」と名付けられました。お転婆でおしゃべりが大好き！">
     <title>まどろむ猫ミコニ</title>
-    <link rel="stylesheet" type="text/css" href="./css/reset.css">
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="./js/functions.js"></script>
+    <?php include('./controller/header.php'); ?>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -28,7 +30,7 @@
     </p>
     <p>
         <a href="https://www.youtube.com/watch?v=QEOJQB7Cj6s">
-            <img src="./img/352.jpg">
+            <img src="./view/img/352.jpg">
         </a>
     </p>
 </body>
