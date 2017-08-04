08010f6564e8a228dfbc1e2ac3099fca7e31669d
KOCHANG-PC\Kota <kochang2014@gmail.com>
2017-08-04 20:16:03 +0900

    CSS/JSを簡易的に設置 - normalize.css,jquery3.2.1.minを一緒に導入

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta name="keywords" content="352, 猫, ネコ, ミコニ, 大塚, 保護, まどろむ, ねこ, 三毛猫, ネコちゃん">
     <meta name="description" content="保護猫カフェ大塚シェルターで出会った、かわいい三毛猫ちゃんを保護して１年になります。352番の札をつけられていたことから「ミコニ」と名付けられました。お転婆でおしゃべりが大好き！">
     <title>まどろむ猫ミコニ</title>
+    <link rel="stylesheet" type="text/css" href="./css/reset.css">
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+    <script type="text/javascript" src="./js/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="./js/functions.js"></script>
     <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -16,7 +20,7 @@
     </script>
 </head>
 <body>
-    <h1 style="background: #f7f7f7; border-left: solid 4px #ccc; padding-left: 0.5em;">352 猫 ネコ ミコニ 大塚 保護 まどろむ ねこ 三毛猫 ネコちゃん</h1>
+    <h1 class="h1-title">352 猫 ネコ ミコニ 大塚 保護 まどろむ ねこ 三毛猫 ネコちゃん</h1>
     <p><a href="https://www.youtube.com/watch?v=QEOJQB7Cj6s">ミコニ</a>352 猫 ネコ 大塚 保護 まどろむ ねこ 三毛猫 ネコちゃん</p>
     <p>
         保護猫カフェ大塚シェルターで出会った、かわいい三毛猫ちゃんを保護して１年になります。<br>
